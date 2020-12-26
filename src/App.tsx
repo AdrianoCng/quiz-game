14dfba3513d02d3398730133567e4221e432966c
Adriano Cangiamila <adrianocangiamila@gmail.com>
2020-12-26 18:28:53 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// components
+import Settings from './components/Settings';
+
+// Types & Utils
+import { Difficulty, Category, Question, fetchQuestions } from './Utils';
+
 function App() {
+  const [difficulty, setDifficulty] = useState(Difficulty.EASY);
+  const [category, setCategory] = useState(Category.GENERAL_KNOWLEDGE);
+  const [questions, setQuestions] = useState<Question[]>([]);
+  const [isStarted, setIsStarted] = useState(false);
+
+  useEffect(() => {
+    console.log(category);
+  }, [questions, category])
+
+  const startTrivia = async () => {
+    setIsStarted(true);
+    const data = await fetchQuestions(difficulty, category);
+    setQuestions(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quiz Game</h1>
+      {!isStarted
+      ? 
+      <div>
+        <Settings setDifficulty={setDifficulty} setCategory={setCategory} category={category}/>
+        <button className="btn btn-primary" onClick={startTrivia}>Start</button>
+      </div> 
+      :
+      questions.length === 0 ?
+      <p>Loading Questions...</p>
+      :
+      questions.map((question, index) => <p key={index} dangerouslySetInnerHTML={{__html: question.question}}></p>)
+      }
     </div>
   );
 }
